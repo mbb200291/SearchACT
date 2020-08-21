97b4c1a986e474fac336663a77d8ef81ead809f3
MBB <MBB200291@users.noreply.github.com>
2020-08-21 15:26:43 +0800

    Add files via upload

---
+++ SearchACT.py
@@ -0,0 +1,183 @@
+ #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug  3 23:52:45 2019
+
+@author: linbangqi
+
+# Version log:
+    - 1.0.0
+    - 1.1.0:
+       - organize by id to value
+       - include more keys
+    - 1.3.0   
+    - 1.3.1
+    - 1.4.0: add calculator
+    - 1.4.1: Variable name changes; fix width of output table column
+
+# next version
+- include removing function
+
+"""
+
+__version__ = '1.5.0'
+
+
+import pickle
+from sys import argv
+from os import path
+
+#import sys
+
+def save_obj_to_pickle(path, obj): # will end with pickle
+    print('# saving file to:', '%s.pickle'%path)
+    with open('%s.pickle'%path, 'wb') as f_pkl:
+        pickle.dump(obj, f_pkl)
+
+def read_pickle(path_):
+    with open(path_, 'rb') as file:
+        return pickle.load(file)
+
+
+PATH_OF_SCRIPT = path.dirname(argv[0])
+PATH_DICT_KEY_CONTACT = path.join(PATH_OF_SCRIPT, r'_DICT_KEY_CONTACTs.pickle')
+PATH_DICT_TERM_KEY = path.join(PATH_OF_SCRIPT, '_dict_terms_key.pickle')
+DICT_KEY_CONTACT = read_pickle(PATH_DICT_KEY_CONTACT)
+DICT_TERM_KEY = read_pickle(PATH_DICT_TERM_KEY)
+
+
+def add_searchTerm_to_key():
+    #from os import path
+    #global DICT_KEY_CONTACT
+    #global DICT_TERM_KEY
+    str_input_KEY = input('Type the E-mail ID (for example, type "benlin" for "benlin@actgenomics.com") or type "exit" to exit.\n>>> ').lower()
+    if str_input_KEY in ['EX', 'exit', 'EXIT', 'QUIT']:
+        return 0
+    elif str_input_KEY not in DICT_KEY_CONTACT.keys():
+        print('*** The ID not exist. ***')
+        return 0
+    str_input_Term = input('Type the new search term or type "exit" to exit.\n>>> ').lower()
+    if str_input_Term in ['EX', 'exit', 'EXIT', 'QUIT']:
+        return 0
+        
+    DICT_TERM_KEY.setdefault(str_input_Term.lower(), set()).add(str_input_KEY)
+    save_obj_to_pickle(path.splitext(PATH_DICT_TERM_KEY)[0], DICT_TERM_KEY)
+    print(f'Successfuly add new search terms. "{str_input_Term}":"{str_input_KEY}"')
+    return 0
+
+def add_contactInfo():
+    #from os import path
+    str_input_KEY = input('Type the E-mail ID (for example, type "benlin" for "benlin@actgenomics.com") or type "exit" to exit.\n>>> ').lower()
+    if str_input_KEY in ['EX', 'exit', 'EXIT', 'QUIT']:
+        return 0
+    elif str_input_KEY not in DICT_KEY_CONTACT.keys():
+        print('*** The ID not exist. ***')
+        return 0
+    
+    str_add_info = input('Type the new info of the person or type "exit" to exit.\n>>> ').lower()
+    if str_add_info in ['EX', 'exit', 'EXIT', 'QUIT']:
+        return 0
+    DICT_KEY_CONTACT[str_input_KEY].append(str_add_info)
+    save_obj_to_pickle(path.splitext(PATH_DICT_KEY_CONTACT)[0], DICT_KEY_CONTACT)
+    print(f'Successfuly add new info terms. "{DICT_KEY_CONTACT[str_input_KEY]}"')
+    return 0
+def raw_match(str_input, DICT_TERM_KEY, DICT_KEY_CONTACT):
+    ## partial matches
+    temp_collect = set()
+    for i in DICT_TERM_KEY.keys():
+        if i.startswith(str_input):
+            for k in DICT_TERM_KEY[i]:
+                temp_collect.add(k)
+    for r in temp_collect:
+        print('\n', '>'+'\t'.join(DICT_KEY_CONTACT[r]))
+        
+    ## within name matches
+    print('\n---------------------')
+    temp_collect2 = set()
+    for i in DICT_TERM_KEY.keys():
+        if str_input in i:
+            for k in DICT_TERM_KEY[i]:
+                temp_collect2.add(k)
+    for r in temp_collect2:
+        print('\n', '>'+'\t'.join(DICT_KEY_CONTACT[r]))
+    if temp_collect==set() and temp_collect2==set():
+        print(f'\n "{str_input}" not in list. or type "exit" to exit')
+    
+# main process
+def main():
+#    global DICT_KEY_CONTACT, DICT_TERM_KEY,  PATH_DICT_KEY_CONTACT, PATH_DICT_TERM_KEY
+
+    
+    while True:
+        str_input = input('\n>>> ').lower()
+        if str_input in DICT_TERM_KEY:
+            ## matched
+            for k in DICT_TERM_KEY[str_input]:
+                print('\n', '>'+'\t'.join(DICT_KEY_CONTACT[k]))
+        
+        # add search term function
+        elif str_input in ['*addterm', '*add1']:
+            print('into adding mode')
+            ans = add_searchTerm_to_key()
+            if ans == 0:
+                pass
+            else:
+                break
+
+        # add contact info function
+        elif str_input in ['*addinfo', '*add2']:
+            print('into editing mode')
+            ans = add_contactInfo()
+            if ans == 0:
+                pass
+            else:
+                break
+        # remove contact info function
+        elif str_input in ['*rminfo', '*rm2']:
+            pass
+            """
+            print('into editing mode')
+            ans = add_contactInfo()
+            if ans == 0:
+                pass
+            else:
+                break
+            """
+        
+        elif str_input in ['*rebuild', '*rb']:
+            pass
+            '''
+            import updata_contact
+            print('** this operation will wipe out the term created by your own. Sure?') 
+            str_input = input('\n(Y/N) >>> ').lower()
+            if str_input == 'y':
+                updata_contact.MakeDict(updata_contact.locate_contact_file())
+                DICT_KEY_CONTACT = read_pickle(PATH_DICT_KEY_CONTACT)
+                DICT_TERM_KEY = read_pickle(PATH_DICT_TERM_KEY)
+            else:
+                pass
+            '''
+        elif str_input.startswith('$'):
+            print(eval(str_input[1:]))
+        elif str_input in ['exit', 'bye', 'ex', 'bye bye', 'quit']:
+            break
+        elif str_input in ['hi', 'hello']:
+            print('HI')
+            #break
+        # '''
+        # elif '+' in str_input:
+            str_input = input('\nCalculator >>> ')
+            # try:
+                # print(eval(str_input))
+            # except:
+                # print('*** error ***')
+        # '''
+        elif str_input in ['*help', '*h']:
+            print('\n# Type "*addterm", "*add1" to add search term linked to someone. \n# Type "*addinfo" or "*add2" to add additional info to someone. \n# Type "exit", "ex" to leave.')
+        else:
+            raw_match(str_input, DICT_TERM_KEY, DICT_KEY_CONTACT)
+
+
+if __name__ == '__main__':
+    print('Type part of name to search ACT contact. Type "*help" to get detail instruction. Type "exit" to leave.')
+    main()
---
+++ UpdataContact.py
@@ -0,0 +1,140 @@
+'''
+
+version:
+- 1.3.0:
+ - add phone number query key
+- 1.4.0:
+ - add department name
+ -1.5.1:
+ - new rule
+ - 1.6.0: update contact by searching contact file("行動基因通訊錄") within same folder of this script
+ - 1.7.0: 
+ 
+next version:
+ - keep customize content
+ 
+'''
+
+
+import pandas as pd
+from sys import argv
+from os import path, listdir, splitext
+import pickle
+
+__version__ = '1.7.0'
+
+def save_obj_to_pickle(path_, obj): #will end with pickle
+    print('# saving file to:', '%s.pickle'%path_)
+    with open('%s.pickle'%path_, 'wb') as f_pkl:
+        pickle.dump(obj, f_pkl)
+def read_pickle(path_):
+    with open(path_, 'rb') as file:
+        return pickle.load(file)
+
+def locate_contact_file():
+    cwd = path.dirname(argv[0])
+    path_contact = sorted([x for x in listdir(cwd) if ".xlsx" in x and not x.startswith('~$')])[-1]
+    #print([x for x in os.listdir(cwd) if ".xlsx" in x])
+    print('# Read file:', path_contact )
+    return path.join(cwd, path_contact)
+        
+def remove_blank(tab):
+    #First, find NaN entries in first column
+    blank_row_bool = tab.iloc[:,4].isna()
+    #Next, get index of first NaN entry
+    blank_row_index =  [i for i, x in enumerate(blank_row_bool) if x] [0]
+    #Finally, restrict dataframe to rows before the first NaN entry
+    return tab.iloc[:(blank_row_index)]
+    
+def read_excel(path_contact):
+    if path.isfile(path_contact):
+        #contacts = pd.read_excel(path_contact, nrows=nrows)
+        contacts = remove_blank(pd.read_excel(path_contact))
+        contacts.dropna(how="all", inplace=True)
+#        contacts = contacts.loc[(~contacts['信     箱'].isna()) & (contacts['信     箱'].str.contains("@"))]
+        return contacts, splitext(path_contact)[-1]
+    else:
+        print('*** Contact file not exist')
+        return None
+        
+def MakeDict(path_contact):#, _nrows):
+    ## Read excel
+    contacts, excel_file_name = read_excel(path_contact)
+    if type(contacts)!=pd.core.frame.DataFrame:
+        return None
+    ## Make dictionary
+    dict_to_PhoneNum = {} # 
+    dict_PhoneNum_Info = {'*version':excel_file_name, '*ver':excel_file_name}
+    for i,R in contacts.iterrows():
+        print(R.姓名)
+        #print(R['英 文 名'])
+        ChnName = R.姓名
+        EngName = str(R['英 文 名'])
+        EngName_dd = EngName.replace('-', ' ')
+        EngName_dd2 = EngName.replace('-', '')
+        MailAress = R['信     箱']
+        PhoneNum = R.分機
+        CellPhone = R['手   機']
+        Department = R['部門名稱']
+        
+        KEY = MailAress.split('@')[0]
+        
+        ## first part: key to result
+        dict_PhoneNum_Info[KEY] = [str(x) for x in [ChnName, EngName, Department, MailAress, PhoneNum, CellPhone]]
+
+        ## second part: query items to key
+        dict_to_PhoneNum.setdefault(ChnName, set()).add(KEY)
+        dict_to_PhoneNum.setdefault(ChnName[1:], set()).add(KEY)
+        dict_to_PhoneNum.setdefault(ChnName[:-1], set()).add(KEY)
+        dict_to_PhoneNum.setdefault(ChnName[0], set()).add(KEY)
+        #
+        dict_to_PhoneNum.setdefault(EngName.lower(), set()).add(KEY)
+        dict_to_PhoneNum.setdefault(' '.join(EngName.lower().split(' ')[:-1]), set()).add(KEY)
+        dict_to_PhoneNum.setdefault(EngName.split(' ')[-1].lower(), set()).add(KEY)
+        for i in range(len(EngName.split(' '))):
+            dict_to_PhoneNum.setdefault(EngName.split(' ')[i].lower(), set()).add(KEY)
+        for i in range(len(EngName.split('-'))):
+            dict_to_PhoneNum.setdefault(EngName.split('-')[i].lower(), set()).add(KEY)
+        dict_to_PhoneNum.setdefault(EngName_dd.lower(), set()).add(KEY)
+        dict_to_PhoneNum.setdefault(' '.join(EngName_dd.lower().split(' ')[:-1]), set()).add(KEY)
+        dict_to_PhoneNum.setdefault(EngName_dd.split(' ')[-1].lower(), set()).add(KEY)
+        dict_to_PhoneNum.setdefault(EngName_dd2.lower(), set()).add(KEY)
+        dict_to_PhoneNum.setdefault(' '.join(EngName_dd2.lower().split(' ')[:-1]), set()).add(KEY)
+        dict_to_PhoneNum.setdefault(EngName_dd2.split(' ')[-1].lower(), set()).add(KEY)
+        dict_to_PhoneNum.setdefault(''.join(EngName.lower().split(' ')[:-1]), set()).add(KEY)
+        dict_to_PhoneNum.setdefault(EngName_dd2[:1].lower(), set()).add(KEY)
+        dict_to_PhoneNum.setdefault(EngName_dd2[:2].lower(), set()).add(KEY)
+        dict_to_PhoneNum.setdefault(EngName_dd2[:3].lower(), set()).add(KEY)
+        dict_to_PhoneNum.setdefault(EngName_dd2[:4].lower(), set()).add(KEY)
+        dict_to_PhoneNum.setdefault(EngName_dd2[:5].lower(), set()).add(KEY)
+        #
+        dict_to_PhoneNum.setdefault(MailAress.split('@')[0].lower(), set()).add(KEY)
+        # by Phone num
+        dict_to_PhoneNum.setdefault(str(PhoneNum), set()).add(KEY)
+        # by cell phone num
+        dict_to_PhoneNum.setdefault(str(CellPhone), set()).add(KEY)
+        # by department
+        dict_to_PhoneNum.setdefault(str(Department), set()).add(KEY)
+
+
+    #print(os.path.split(sys.path[0])[0])
+    path_term_key = path.join(path.dirname(path_contact), '_dict_terms_key')
+    path_key_contact = path.join(path.dirname(path_contact), '_dict_key_contacts')
+    save_obj_to_pickle(path_term_key, dict_to_PhoneNum)
+    save_obj_to_pickle(path_key_contact, dict_PhoneNum_Info)
+    '''
+    if regenerate_dict:
+        save_obj_to_pickle(path_term_key, dict_to_PhoneNum)
+        save_obj_to_pickle(path_key_contact, dict_PhoneNum_Info)
+    else:
+        save_obj_to_pickle(path_term_key, read_pickle(path_term_key).update(dict_to_PhoneNum))
+        save_obj_to_pickle(path_key_contact, read_pickle(path_key_contact).update(dict_PhoneNum_Info))
+    '''
+    
+
+def main():
+    print('# Loading')
+    MakeDict(locate_contact_file())# int(sys.argv[2]))
+    
+if __name__ == '__main__':
+    main()
