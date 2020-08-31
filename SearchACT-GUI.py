4779b1875f6876264b6c61285b2f5dea4d4109a4
linkuanhang <linkuanhang@yahoo.com.tw>
2020-08-31 23:07:41 +0800

    Modify GUI framework

--- SearchACT-GUI.py
+++ SearchACT-GUI.py
@@ -13,95 +13,41 @@ dmapPath = os.path.join(rootPath, '_dict_terms_key.pickle')
 with open(dmapPath, 'rb') as file:
     dmap = pickle.load(file)
 
-class Widget(object):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.create()
+class SearchACTModel:
+    def __init__(self, data, dmap):
+        self.data = data
+        self.dmap = dmap
 
-    def create(self):
-        raise NotImplementedError()
-
-    def update(self):
-        raise NotImplementedError()
-
-class SearchACT(Widget, tkinter.Tk):
-    def __init__(self):
-        super().__init__()
-
-    def create(self):
-        self.title('SearchACT')
-        self.geometry('800x600')
-        self.container = RootFrame(self)
-        self.container.pack(fill='both', padx=5, pady=5, expand=True)
-        self.container.inputContainer.search['command'] = self.search
-        self.container.inputContainer.calculate['command'] = self.calculate
-
-    def search(self):
-        text = self.container.inputContainer.text.get().lower()
+    def search(self, text):
         ndata = []
-        if text in dmap:
-            for key in dmap[text]:
+        if text in self.dmap:
+            for key in self.dmap[text]:
                 ndata.append(data[key])
         else:
             dataMatch = set()
-            for kwarg in dmap:
+            for kwarg in self.dmap:
                 if text in kwarg:
-                    for key in dmap[kwarg]:
+                    for key in self.dmap[kwarg]:
                         dataMatch.add(key)
             for key in dataMatch:
-                ndata.append(data[key])
+                ndata.append(self.data[key])
         if (len(ndata) == 0):
-            self.container.outputContainer.setData([[f'"{text}" not in list.']])
+            return [[f'"{text}" not in list.']]
         else:
-            self.container.outputContainer.setData(ndata)
+            return ndata
 
-    def calculate(self):
-        text = self.container.inputContainer.text.get()
+    def calculate(self, text):
         try:
             result = eval(text)
-            self.container.outputContainer.setData([[str(result)]])
+            return [[str(result)]]
         except:
-            self.container.outputContainer.setData([['Formula error!']])
+            return [['Formula error!']]
 
-
-class RootFrame(Widget, tkinter.Frame):
-    def create(self):
-        self.inputContainer = InputFrame(self)
-        self.outputContainer = OutputFrame(self)
-        self.inputContainer.pack(fill='x')
-        self.outputContainer.pack(fill='both', expand=True)
-
-class InputFrame(Widget, tkinter.Frame):
-    def create(self):
-        self.label = tkinter.Label(self, text='Input:')
-        self.text = tkinter.Entry(self)
-        self.search = tkinter.Button(self, text='Search')
-        self.calculate = tkinter.Button(self, text='Calculate')
-        self.label.pack(side='left')
-        self.calculate.pack(side='right')
-        self.search.pack(side='right')
-        self.text.pack(side='left', fill='x', expand=True)
-
-class OutputFrame(Widget, tkinter.Frame):
+class ScrollTable(tkinter.Frame):
     def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.data = [['Empty']]
         self.labels = []
-        super().__init__(*args, **kwargs)
-
-    def onFrameConfigure(self, event):
-        self.canvas.configure(scrollregion=self.canvas.bbox('all'))
-
-    def onCanvasConfigure(self, event):
-        self.canvas.itemconfig(self.canvas_window, width=event.width)
-
-    def onMouseWheel(self, event):
-        if event.num == 4 or event.delta > 0:
-            self.canvas.yview('scroll', -1, 'units')
-        elif event.num == 5 or event.delta < 0:
-            self.canvas.yview('scroll', 1, 'units')
-
-    def create(self):
-        self.configure(bg='white')
         self.canvas = tkinter.Canvas(self)
         self.view = tkinter.Frame(self.canvas)
         self.vsb = tkinter.Scrollbar(self, orient='vertical', command=self.canvas.yview)
@@ -114,6 +60,18 @@ class OutputFrame(Widget, tkinter.Frame):
         self.vsb.pack(side='right', fill='y')
         self.canvas.pack(fill='both', expand=True)
         self.update()
+
+    def onFrameConfigure(self, event):
+        self.canvas.configure(scrollregion=self.canvas.bbox('all'))
+
+    def onCanvasConfigure(self, event):
+        self.canvas.itemconfig(self.canvas_window, width=event.width)
+
+    def onMouseWheel(self, event):
+        if event.num == 4 or event.delta > 0:
+            self.canvas.yview('scroll', -1, 'units')
+        elif event.num == 5 or event.delta < 0:
+            self.canvas.yview('scroll', 1, 'units')
 
     def update(self):
         for label in self.labels:
@@ -129,7 +87,43 @@ class OutputFrame(Widget, tkinter.Frame):
         self.data = data
         self.update()
 
+class SearchACTView(tkinter.Tk):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.title('SearchACT')
+        self.geometry('800x600')
+        self.container = tkinter.Frame(self)
+        self.container.pack(fill='both', padx=5, pady=5, expand=True)
+        self.inputContainer = tkinter.Frame(self.container)
+        self.inputContainer.pack(fill='x')
+        self.inputLabel = tkinter.Label(self.inputContainer, text='Input:')
+        self.inputText = tkinter.Entry(self.inputContainer)
+        self.searchButton = tkinter.Button(self.inputContainer, text='Search')
+        self.calculateButton = tkinter.Button(self.inputContainer, text='Calculate')
+        self.inputLabel.pack(side='left')
+        self.calculateButton.pack(side='right')
+        self.searchButton.pack(side='right')
+        self.inputText.pack(side='left', fill='x', expand=True)
+        self.outputContainer = ScrollTable(self.container)
+        self.outputContainer.pack(fill='both', expand=True)
+
+class SearchACTController:
+    def __init__(self):
+        self.model = SearchACTModel(data, dmap)
+        self.view = SearchACTView()
+        self.view.searchButton['command'] = self.search
+        self.view.calculateButton['command'] = self.calculate
+        self.view.mainloop()
+
+    def search(self):
+        text = self.view.inputText.get().lower()
+        result = self.model.search(text)
+        self.view.outputContainer.setData(result)
+
+    def calculate(self):
+        text = self.view.inputText.get()
+        result = self.model.calculate(text)
+        self.view.outputContainer.setData(result)
 
 if __name__ == '__main__':
-    app = SearchACT()
-    app.mainloop()
+    app = SearchACTController()
