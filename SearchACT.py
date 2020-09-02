e7ac4b5827111f6f5ad7d30be562941d003f8866
Shau <b97390022@gmail.com>
2020-09-02 11:45:13 +0800

    fix cal

--- SearchACT.py
+++ SearchACT.py
@@ -31,6 +31,7 @@ __version__ = '1.5.2'
 import pickle
 from sys import argv
 from os import path
+import time
 
 def save_obj_to_pickle(path, obj): # will end with pickle
     print('# saving file to:', '%s.pickle'%path)
@@ -106,10 +107,12 @@ def raw_match(str_input, DICT_TERM_KEY, DICT_KEY_CONTACT):
         print(f'\n "{str_input}" not in list. or type "exit" to exit')
 
 def cal(s,lop):
+    s = handle(s)
     if not any(x in s for x in ['+','-','*', '/']):
         return float(s)
     for i in ['+','-','*', '/']:
         left, op, right = s.partition(i)
+        # print(i, [left, op, right])
         if op in ['+', '-','*','/']:
             if op == '*':
                 if right == '':
@@ -117,19 +120,22 @@ def cal(s,lop):
                 else:
                     return(cal(left,lop) * cal(right,lop))
             elif op == '/':
-                if lop == '':
+                if lop != '/':
                     lop = '/'
                 elif lop == '/':
                     return(cal(left,lop) * cal(right,lop))   
                     lop = ''
                 return(cal(left,lop) / cal(right,lop))
             elif op == '+':
-                return(cal(left,lop) + cal(right,lop))
+                if left == '':
+                    return(cal('0+'+right,lop))
+                else:
+                    return(cal(left,lop) + cal(right,lop))
             elif op == '-':
                 if left == '':
                     return(cal('0-'+right,lop))
                 else:
-                    if lop == '':
+                    if lop != '-':
                         lop = '-'
                     elif lop == '-':
                         return(cal(left,lop) + cal(right,lop))   
@@ -152,20 +158,47 @@ def parse(s,lop=''):
                     return('left bracket first!')
                 else:
                     r.append(p)
-                    ans = cal(s[l[-1]+1:r[-1]],lop)
+                    if s[l[-1]+1:r[-1]] == '':
+                        return 'formula error!'
+                    else:
+                        ans = cal(s[l[-1]+1:r[-1]],lop)
                     s = "".join((s[:l[-1]],str(ans),s[r[-1]+1:]))
-                    l.pop()
+                    l.clear()
                     r.pop()
                     break
-    if '+-' in s:
-        s=s.replace('+-','-')
-    elif '--' in s:
-        s=s.replace('--','+')
-    elif '*-' in s:
-        s="".join(('-',s.replace('*-','*')))
-    elif '/-' in s:
-        s="".join(('-',s.replace('/-','/')))
+    s = handle(s)
     return str(cal(s,lop))
+
+def handle(s):  
+    timestart = time.time()
+    while any(x in s for x in ['+-','--','*-', '/-']) and time.time() < timestart+5:
+        if '+-' in s:
+            s=s.replace('+-','-')
+        elif '--' in s:
+            if s.find('--') == 0:
+                s=s.replace('--','')
+            s=s.replace('--','+')
+        elif '*-' in s:
+            p = s.find('*-')
+            for i in range(p-1,-1,-1):
+                if s[i] in ['+','-','*','/']: 
+                    s="".join((s[:i+1],'-',s[i+1:]))
+                    s="".join((s[:p+1],'*',s[p+3:]))
+                    break
+                elif i == 0:
+                    s="".join(('-',s[i:]))
+                    s="".join((s[:p+1],'*',s[p+3:]))
+        elif '/-' in s:
+            p = s.find('/-')
+            for i in range(p-1,-1,-1):
+                if s[i] in ['+','-','*','/']: 
+                    s="".join((s[:i+1],'-',s[i+1:]))
+                    s="".join((s[:p+1],'/',s[p+3:]))
+                    break
+                elif i == 0:
+                    s="".join(('-',s[i:]))
+                    s="".join((s[:p+1],'/',s[p+3:]))
+    return(s)
 
 # main process
 def main():
