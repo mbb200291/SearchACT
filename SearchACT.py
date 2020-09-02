90ec11309e477713df1a1fc325628068ee37930f
BOSHAU CHEN <b97390022@gmail.com>
2020-09-02 22:22:52 +0800

    fix cal, substract caculation to caculation.py

--- SearchACT.py
+++ SearchACT.py
@@ -36,6 +36,8 @@ from os import path
 import time
 ### coustom functions
 from modules.tools import save_obj_to_pickle, read_pickle
+# caculator
+from modules.calculator import cal,parse,handle
 ###
 
 PATH_OF_SCRIPT = path.dirname(argv[0])
@@ -141,141 +143,6 @@ def parse_formula(s):
             temp += e
     li_element.append(temp)    
     return cal_multi_condi_li(li_element)
-
-""" # ben's modified --> fix some issue 
-
-def cal(s):
-    s = s.replace(' ', '')
-    if not any(x in s for x in ['+','-','*', '/']):
-        return float(s)
-    for i in [x for x in s if x in ['+','-','*', '/']]:
-        left, op, right = s.partition(i)
-        if op in ['*', '/','+','-']: 
-            if op == '*':
-                return(cal(left) * cal(right))
-            elif op == '/':
-                return(cal(left) / cal(right))
-            elif op == '+':
-                return(cal(left) + cal(right))
-            elif op == '-':
-                return(cal(left) - cal(right))
-def parse(s):
-    if s == '':
-        return 'Empty string!'
-    l = []
-    r = []
-    s = s.replace(' ','')
-    
-    for p in range(len(s)):
-        if any(x in s for x in ['(', ')']):   
-            if s[p] == '(':
-                l.append(p)
-            elif s[p] == ')':
-                if len(l) == 0:
-                    print('left bracket first')
-                else:
-                    r.append(p)
-                    ans = cal(s[l[-1]+1:r[-1]])
-                    len_bra = r[-1] - l[-1] +1
-                    s = "".join((s[:l[-1]], str(ans)+' '*(len_bra-len(str(ans))), s[r[-1]+1:]))
-                    l.pop()
-                    r.pop()
-        
-    return str(cal(s))
-"""
-def cal(s,lop):
-    s = handle(s)
-    if not any(x in s for x in ['+','-','*', '/']):
-        return float(s)
-    for i in ['+','-','*', '/']:
-        left, op, right = s.partition(i)
-        # print(i, [left, op, right])
-        if op in ['+', '-','*','/']:
-            if op == '*':
-                if right == '':
-                    return(cal(left,lop))
-                else:
-                    return(cal(left,lop) * cal(right,lop))
-            elif op == '/':
-                if lop != '/':
-                    lop = '/'
-                elif lop == '/':
-                    return(cal(left,lop) * cal(right,lop))   
-                    lop = ''
-                return(cal(left,lop) / cal(right,lop))
-            elif op == '+':
-                if left == '':
-                    return(cal('0+'+right,lop))
-                else:
-                    return(cal(left,lop) + cal(right,lop))
-            elif op == '-':
-                if left == '':
-                    return(cal('0-'+right,lop))
-                else:
-                    if lop != '-':
-                        lop = '-'
-                    elif lop == '-':
-                        return(cal(left,lop) + cal(right,lop))   
-                        lop = ''
-                    return(cal(left,lop) - cal(right,lop))
-
-
-def parse(s,lop=''):
-    if s == '':
-        return 'Empty string!'
-    l = []
-    r = []
-    s = s.replace(' ','')
-    while any(x in s for x in ['(',')']):
-        for p in range(len(s)):
-            if s[p] == '(' and len(r) < 1:
-                l.append(p)
-            elif s[p] == ')':
-                if len(l) == 0:
-                    return('left bracket first!')
-                else:
-                    r.append(p)
-                    if s[l[-1]+1:r[-1]] == '':
-                        return 'formula error!'
-                    else:
-                        ans = cal(s[l[-1]+1:r[-1]],lop)
-                    s = "".join((s[:l[-1]],str(ans),s[r[-1]+1:]))
-                    l.clear()
-                    r.pop()
-                    break
-    s = handle(s)
-    return str(cal(s,lop))
-
-def handle(s):  
-    timestart = time.time()
-    while any(x in s for x in ['+-','--','*-', '/-']) and time.time() < timestart+5:
-        if '+-' in s:
-            s=s.replace('+-','-')
-        elif '--' in s:
-            if s.find('--') == 0:
-                s=s.replace('--','')
-            s=s.replace('--','+')
-        elif '*-' in s:
-            p = s.find('*-')
-            for i in range(p-1,-1,-1):
-                if s[i] in ['+','-','*','/']: 
-                    s="".join((s[:i+1],'-',s[i+1:]))
-                    s="".join((s[:p+1],'*',s[p+3:]))
-                    break
-                elif i == 0:
-                    s="".join(('-',s[i:]))
-                    s="".join((s[:p+1],'*',s[p+3:]))
-        elif '/-' in s:
-            p = s.find('/-')
-            for i in range(p-1,-1,-1):
-                if s[i] in ['+','-','*','/']: 
-                    s="".join((s[:i+1],'-',s[i+1:]))
-                    s="".join((s[:p+1],'/',s[p+3:]))
-                    break
-                elif i == 0:
-                    s="".join(('-',s[i:]))
-                    s="".join((s[:p+1],'/',s[p+3:]))
-    return(s)
 
 # main process
 def main():
---
+++ modules/calculator.py
@@ -0,0 +1,75 @@
+def cal(s, lop):
+    s = handle(s)
+    if not any(x in s for x in ['+', '-', '*', '/']):
+        return float(s)
+    for i in ['+', '-', '*', '/']:
+        left, op, right = s.partition(i)
+        # print(i, [left, op, right])
+        if op in ['+', '-', '*', '/']:
+            if op == '*':
+                return cal(left,lop) * cal(right,lop)
+            elif op == '/':
+                if lop == '/':
+                    return cal(left, '') * cal(right, '/')
+                return cal(left, '') / cal(right, '/')
+            elif op == '+':
+                if left == '':
+                    return cal(f'0+{right}', '')
+                else:
+                    return cal(left, '') + cal(right, '')
+            elif op == '-':
+                if left == '':
+                    return cal(f'0-{right}', '')
+                else:
+                    if lop == '-':
+                        return cal(left, '') + cal(right, '-')
+                    return cal(left, '') - cal(right, '-')
+
+def parse(s):
+    if s == '':
+        return 'Empty string!'
+    q = -1
+    s = s.replace(' ','')
+    while any(x in s for x in ['(',')']):
+        for p in range(len(s)):
+            if s[p] == '(':
+                q = p
+            elif s[p] == ')':
+                if q < 0:
+                    return 'Left bracket first!'
+                else:
+                    if s[q+1:p] == '':
+                        return 'Formula error!'
+                    else:
+                        ans = cal(s[q+1:p], '')
+                    s = f'{s[:q]}{ans}{s[p+1:]}'
+                    q = -1
+                    break
+    return str(cal(s, ''))
+
+def handle(s):  
+    while any(x in s for x in ['+-', '--', '*-', '/-']):
+        if '+-' in s:
+            s = s.replace('+-', '-')
+        elif '--' in s:
+            s = s.replace('--', '+')
+        elif '*-' in s:
+            p = s.find('*-')
+            for i in range(p-1, -1, -1):
+                if s[i] in ['+', '-', '*', '/']:
+                    s = f'{s[:i+1]}-{s[i+1:p]}*{s[p+2:]}'
+                    break
+                elif i == 0:
+                    s = f'-{s[i:p]}*{s[p+2:]}'
+        elif '/-' in s:
+            p = s.find('/-')
+            for i in range(p-1, -1, -1):
+                if s[i] in ['+', '-', '*', '/']:
+                    s = f'{s[:i+1]}-{s[i+1:p]}/{s[p+2:]}'
+                    break
+                elif i == 0:
+                    s = f'-{s[i:p]}/{s[p+2:]}'
+    return s
+
+if __name__ == '__main__':
+    print(parse('+5+6*((25-9/2)-3*5/85/-12-22*5)-53/-2+(-2/(2+5-3*5*-5))'))
